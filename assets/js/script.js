b909dead9d004ac6363fbb63d96c3abd9af208ad
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-08-04 20:03:28 -0400

    console log parsed (stored) response elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,28 @@ searchButton.addEventListener("click",handleCitySearch);
 
 //testFetch();  // TODO - DELIBERATELY disabled
 
+let resp;
+testResponse();  // TODO - temporary
+
+
+function testResponse() {  // TODO - temporary
+    resp = JSON.parse(respJson);
+    let cnt = resp.cnt;
+    if (cnt < 1) {
+        return;
+    }
+    console.log('count = ' + cnt);
+    console.log('arr len = ' + resp.list.length);
+    for (let i=0;i<cnt;i++) {
+        let txt = 'dt/time = ' + resp.list[i].dt_txt + ' : ';
+        let k = resp.list[i].main.temp;
+        let c = k-273.15;
+        let f = c*9.0/5+32;
+        txt += (f + ' deg F');
+        console.log(i + ' : ' + txt);
+    }
+}
+
 function handleCitySearch(event) {
     if (event === null) {
         console.log('click event was null');
@@ -29,32 +51,29 @@ function handleCitySearch(event) {
     }
 }
 
-function testFetch () {
-    // temporary test code to produce a sample of data returned from API for Phila.
-    let baseUrl = 'https://api.openweathermap.org/';
-    let urlSuffix = 'data/2.5/forecast';
-    let lat = '39.97';
-    let lon = '-75.20';
-    let fullUrl = baseUrl + urlSuffix + '?' + 
-        'lat=' + lat + '&lon=' + lon + '&appid=' + OPENWEATHER_API_KEY;
-    console.log('full URL = "' + fullUrl + '"');
-    fetch(fullUrl) 
-        .then(response => response.json())
-        .then(data => {
-        
-            console.log('data = ' + data);
-            return data;
-        })
-        .then(data_obj => {
-            let str = JSON.stringify(data_obj);
-            localStorage.setItem('phila-test-weather',str);
-            let myWindow = window.open("","","width=500,height=300");
-            let doc = myWindow.document;
-            doc.writeln('<html><head><title>temp</title></head>');
-            doc.writeln('<body><pre>');
-            doc.writeln(str);
-            doc.writeln('</pre></body></html>');
-        });
-    
-}
-    /* https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} */
+// function testFetch () {
+//     // temporary test code to produce a sample of data returned from API for Phila.
+//     let baseUrl = 'https://api.openweathermap.org/';
+//     let urlSuffix = 'data/2.5/forecast';
+//     let lat = '39.97';
+//     let lon = '-75.20';
+//     let fullUrl = baseUrl + urlSuffix + '?' + 
+//         'lat=' + lat + '&lon=' + lon + '&appid=' + OPENWEATHER_API_KEY;
+//     console.log('full URL = "' + fullUrl + '"');
+//     fetch(fullUrl) 
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log('data = ' + data);
+//             return data;
+//         })
+//         .then(data_obj => {
+//             let str = JSON.stringify(data_obj);
+//             localStorage.setItem('phila-test-weather',str);
+//             let myWindow = window.open("","","width=500,height=300");
+//             let doc = myWindow.document;
+//             doc.writeln('<html><head><title>temp</title></head>');
+//             doc.writeln('<body><pre>');
+//             doc.writeln(str);
+//             doc.writeln('</pre></body></html>');
+//         });
+// }
