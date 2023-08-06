ee2b88f09082570f0f420eb8408e4c396346f0b1
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-08-06 17:44:21 -0400

    geo API works, but promise/fetch logic blows my mind and is broken

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,27 +6,27 @@ let searchButton = document.querySelector("#search-button");
 searchButton.addEventListener("click",handleCitySearch);
 
 let resp;
-testResponse();  // TODO - temporary
+//testResponse();  // TODO - temporary
 
 
-function testResponse() {  // TODO - temporary
-    resp = JSON.parse(respJson);
-    let cnt = resp.cnt;
-    if (cnt < 1) {
-        return;
-    }
-    console.log('count = ' + cnt);
-    console.log('arr len = ' + resp.list.length);
-    for (let i=0;i<cnt;i++) {
-        let txt = 'dt/time = ' + resp.list[i].dt_txt + ' : ';
-        let k = resp.list[i].main.temp;
-        let c = k-273.15;
-        let f = c*9.0/5+32;
-        f = Math.floor(f+0.5);
-        txt += (f + ' deg F');
-        console.log(i + ' : ' + txt);
-    }
-}
+// function testResponse() {  // TODO - temporary
+//     resp = JSON.parse(respJson);
+//     let cnt = resp.cnt;
+//     if (cnt < 1) {
+//         return;
+//     }
+//     console.log('count = ' + cnt);
+//     console.log('arr len = ' + resp.list.length);
+//     for (let i=0;i<cnt;i++) {
+//         let txt = 'dt/time = ' + resp.list[i].dt_txt + ' : ';
+//         let k = resp.list[i].main.temp;
+//         let c = k-273.15;
+//         let f = c*9.0/5+32;
+//         f = Math.floor(f+0.5);
+//         txt += (f + ' deg F');
+//         console.log(i + ' : ' + txt);
+//     }
+// }
 
 function handleCitySearch(event) {
     if (event === null) {
@@ -46,10 +46,59 @@ function handleCitySearch(event) {
                 city = city.trim();
                 console.log('trimmed city = "' + city + '" with length = ' + city.length);
                 // TODO - temp - pending refinement, call testFormatWeather() just to demo display
-                testFormatWeather(JSON.parse(respJson)); // TODO
+                // testFormatWeather(JSON.parse(respJson)); // TODO
+                // let cityData = cityDataFromAPI(city);
+                // if (cityData === null) {
+                //     console.log('null returned from geo');
+                // } else {
+                //     console.log('city returned from geo = ' + cityData);
+                // }
+                cityDataFromAPI(city)
+                    .then(function(cityData) {
+                        console.log('city data after API return = ' + cityData);
+                    })
+                    .catch(function(arg) {
+
+                    });
             }
         }
     }
+}
+
+function cityDataFromAPI(cityName) {
+    // returns promise containing:
+    //  {name: name, lat: <north latitude, lon: <east longitude>}
+    //    or
+    //  null  ??
+    //--
+    // Construct URL
+    let BASE_URL = 'http://api.openweathermap.org/';
+    let GEO_API_BRANCH = 'geo/1.0/direct';
+    let url = BASE_URL + GEO_API_BRANCH + "?" +
+        "q=" + cityName /* TODO */ + "&limit=1" +
+        "&appid=" + OPENWEATHER_API_KEY;
+    console.log('geo url = "' + url + '"');
+    return new Promise(function(resolve,reject) {
+        fetch(url)
+            .then(function(responseJson) {
+                console.log('geo resp raw = ' + responseJson);
+                console.log('type of geo resp raw = ' + typeof responseJson);
+                return responseJson.json();
+            })
+            .then(function (respObj) {
+                console.log('geo resp obj = ' + respObj);
+                console.log('type of geo resp conv = ' + typeof respObj);
+                let lat = respObj[0].lat;
+                let lon = respObj[0].lon;
+                let name = respObj[0].name;
+                console.log('name/lat/lon = ' + name + ', ' + lat + ', ' + lon);
+                return {name: name, lat: lat, lon: lon};
+            }).catch(function(arg) {
+                console.log('geo catch arg = ' + arg);
+                console.log('geo catch clause');
+            })
+        }
+    );
 }
 
 function testFormatWeather(resp) {
